54542d80351a5df5f9f13b8f3c0c42f6b94f3de0
Antonin Bruneau <abruneau@cloudera.com>
2016-06-14 11:49:45 +0200

    move navigation in directive

--- test/spec/controllers/navigation.js
+++ test/spec/controllers/navigation.js
@@ -1,23 +1,23 @@
 'use strict';
 
-describe('Controller: SidebarCtrl', function () {
+describe('Controller: NavigationCtrl', function () {
 
   // load the controller's module
   beforeEach(module('cmpApp'));
 
-  var SidebarCtrl,
+  var NavigationCtrl,
     scope;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
-    SidebarCtrl = $controller('SidebarCtrl', {
+    NavigationCtrl = $controller('NavigationCtrl', {
       $scope: scope
       // place here mocked dependencies
     });
   }));
 
   it('should attach a list of awesomeThings to the scope', function () {
-    expect(SidebarCtrl.awesomeThings.length).toBe(3);
+    expect(NavigationCtrl.awesomeThings.length).toBe(3);
   });
 });

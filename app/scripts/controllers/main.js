b25d5c57dfc70e482183e93c3b1e1f4cac56e778
Antonin Bruneau <abruneau@cloudera.com>
2016-06-30 10:55:08 +0200

    Add todo widget to Dashboard

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,11 +7,43 @@
  * # MainCtrl
  * Controller of the cmpApp
  */
-angular.module('cmpApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+angular.module('cmpApp').controller('MainCtrl', function ($scope, Todos) {
+
+	/*global moment */
+
+	function filterTodos(todos) {
+		var out = [];
+		for (var i in todos) {
+			var todo = todos[i];
+			if (moment(todo.dueDate).isSameOrBefore(moment(), 'day')) {
+				out.push(todo);
+			}
+		}
+		return out;
+	}
+
+
+	var updateTodos = function () {
+		$scope.$apply(function () {
+			$scope.todos = filterTodos(Todos.list);
+		});
+	};
+
+	$scope.todos = [];
+
+	$scope.isLate = function (todo) {
+		if (moment(todo.dueDate).isBefore(moment(), 'day')) {
+			return {'background-color':'#f2dede'};
+		}
+	};
+
+	$scope.toggleCompleted = function (todo, completed) {
+		if (angular.isDefined(completed)) {
+			todo.completed = completed;
+		}
+		Todos.update(todo);
+	};
+
+	Todos.getAll();
+	Todos.registerObserverCallback(updateTodos);
+});

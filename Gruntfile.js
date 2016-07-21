d8b6cb77da29edf6d843e5901c16203365ae769f
Antonin Bruneau <abruneau@cloudera.com>
2016-07-21 17:16:46 +0200

    missed files in precedent commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -207,7 +207,7 @@ module.exports = function (grunt) {
 		wiredep: {
 			app: {
 				src: ['<%= yeoman.app %>/index.html'],
-				ignorePath: /\.\.\//
+				//ignorePath: /\.\.\//
 			},
 			test: {
 				devDependencies: true,
